ae75f77e9ace50054284a10fe5b1e12aa4b5781d
Meghan Martin <59030202+meghanmartin995@users.noreply.github.com>
2020-11-08 13:56:05 -0800

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fa86903b1c8af001eb4cee9",
+				"5fa86903b1c8af001eb4ceeb"
+			]
+		},
+		"5fa86903b1c8af001eb4cee9": {
+			"id": "5fa86903b1c8af001eb4cee9",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fa86903b1c8af001eb4ceeb": {
+			"id": "5fa86903b1c8af001eb4ceeb",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
